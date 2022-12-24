8c24044a505d949fbd7d04be3efbc1db4ea07482
valentinmnaydenov <85516432+valentinmnaydenov@users.noreply.github.com>
2022-12-24 07:31:15 +0200

    Update DocItem.sol

--- contracts/DocItem.sol
+++ contracts/DocItem.sol
@@ -1,24 +1,20 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+ // SPDX-License-Identifier: MIT
+ pragma solidity ^0.8.9;
 
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 
-contract DocItem is ERC721URIStorage{
+contract DocItem is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  constructor() ERC721('NFToken', 'NFT') {}
+  constructor() ERC721('DocItem', 'DI') {}
 
-  function mintItem(address _minter, string calldata _tokenURI) public  
-    returns (uint256)
-    {
+  function mintItem(address _minter, string calldata _tokenURI) public returns (uint256) {
     _tokenIds.increment();
-
     uint256 newItemId = _tokenIds.current();
     _mint(_minter, newItemId);
     _setTokenURI(newItemId, _tokenURI);
-
     return newItemId;
   }
 }

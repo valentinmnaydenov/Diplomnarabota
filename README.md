6d8060a462e4a3602dfd9f08fd90238f8d132993
Martin Dobrev <martindobrev87@gmail.com>
2023-01-03 17:51:32 +0200

    Starter project added

--- README.md
+++ README.md
@@ -1,13 +1,50 @@
-# Sample Hardhat Project
+# 🚗 Sit and Drive App
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+Opinionated minimal boilerplate for starting React projects with Bootstrap and couple more goodies.
 
-Try running some of the following tasks:
+## 💡 Some prerequisites
+
+Node version: `18.6.0`
+
+If you use another version, please use [n](https://github.com/tj/n) to manage.
+
+Highly suggest to use `hardhat-shorthand` package for easier development.
 
 ```shell
-npx hardhat help
-npx hardhat test
-GAS_REPORT=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
+npm install --global hardhat-shorthand
 ```
+
+More info in official [Hardhat docs](https://hardhat.org/hardhat-runner/docs/guides/command-line-completion).
+
+## ⚙️ Install dependencies
+
+```shell
+$ yarn
+```
+
+## ⚙️ Config
+
+Copy the example `.env` file and add the needed credentials.
+
+```shell
+cp example.env .env
+```
+
+Register at [Infura](https://www.infura.io/) ot [Alchemy](https://www.alchemy.com/) to get the `PROVIDER_URL`.
+
+Export your wallet private key to `WALLET_PRIVATE_KEY`.
+
+Register at [Etherscan](https://etherscan.io/) to get `ETHERSCAN_API_KEY`.
+
+Note: The default network is `Sepolia`.
+
+## 🚀 Available Scripts
+
+In the project directory, you can run:
+
+```shell
+$ yarn start
+```
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
--- contracts/Document.sol
+++ contracts/Document.sol
@@ -1,53 +1,52 @@
- // SPDX-License-Identifier: MIT
-pragma solidity ^ 0.8 .9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "./DocItem.sol";
-
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+import './DocItem.sol';
 
 contract Document is ReentrancyGuard {
-    using Counters for Counters.Counter;
-address payable public owner;
-DocItem private immutable docItemContract;
+  using Counters for Counters.Counter;
+  address payable public owner;
+  DocItem private immutable docItemContract;
 
-uint256 nextAvailableId = 0;
+  uint256 nextAvailableId = 0;
 
-enum Status {
+  enum Status {
     Submitted,
     Approved,
     Pending,
     Rejected
-}
+  }
 
-enum Typeofdoc {
+  enum Typeofdoc {
     ID,
     PASSPORT,
     CAR
-}
+  }
 
-enum Eyes {
+  enum Eyes {
     blue,
     green,
     brown
-}
+  }
 
-enum Category {
+  enum Category {
     A,
     B,
     C,
     D
-}
-struct IdCard {
+  }
+  struct IdCard {
     string name;
     uint256 id;
     Typeofdoc typeofdoc;
     Eyes eyes;
     Status status;
     string photo;
-}
+  }
 
-struct CarLicense {
+  struct CarLicense {
     string name;
     uint256 id;
     Typeofdoc typeofdoc;
@@ -55,95 +54,103 @@ struct CarLicense {
     Status status;
     Category category;
     string photo;
-}
+  }
 
-struct Passport {
+  struct Passport {
     string name;
     uint256 id;
     Typeofdoc typeofdoc;
     Eyes eyes;
     Status status;
     string photo;
-}
+  }
 
-struct ApplicationForm {
+  struct ApplicationForm {
     string name;
     Status status;
     uint256 id;
     string ipfsLink;
-}
+  }
 
-
-constructor(address _docItemAddress) {
+  constructor(address _docItemAddress) {
     owner = payable(msg.sender);
     docItemContract = DocItem(_docItemAddress);
-}
+  }
 
-
-modifier onlyOwner() {
-    require(msg.sender == owner, "Only the owner can perform this action");
+  modifier onlyOwner() {
+    require(msg.sender == owner, 'Only the owner can perform this action');
     _;
-}
+  }
 
-mapping(uint256 => IdCard)public idcards;
-mapping(uint256 => CarLicense)public carlicenses;
-mapping(uint256 => Passport)public passports;
-mapping(uint256 => ApplicationForm)public applicationForms;
-mapping(uint256 => Status)public applicationStatus;
-mapping(uint256 => bool)public nfts;
+  mapping(uint256 => IdCard) public idcards;
+  mapping(uint256 => CarLicense) public carlicenses;
+  mapping(uint256 => Passport) public passports;
+  mapping(uint256 => ApplicationForm) public applicationForms;
+  mapping(uint256 => Status) public applicationStatus;
+  mapping(uint256 => bool) public nfts;
 
+  uint256 public idCardCounter = 0;
+  uint256 public idpassportCounter = 0;
+  uint256 public idcarlicenseCounter = 0;
 
-uint public idCardCounter = 0;
-uint public idpassportCounter = 0;
-uint public idcarlicenseCounter = 0;
-
-
-function mint(address _to, uint256 _id)public {
-    require(!nfts[_id], "NFT with this ID already exists");
+  function mint(address _to, uint256 _id) public {
+    require(!nfts[_id], 'NFT with this ID already exists');
     nfts[_id] = true;
-}
-//The mint function checks if an NFT with the given ID already exists. It does this by checking the value of nfts[_id].
-//If nfts[_id] is true, it means that an NFT with this ID already exists and the function will throw an error.
+  }
 
-event Mint(address indexed _to, uint256 indexed _id);
-function mintNFT(ApplicationForm memory applicationForm)public {
-    require(applicationForm.status == Status.Approved, "Invalid approval");
+  //The mint function checks if an NFT with the given ID already exists. It does this by checking the value of nfts[_id].
+  //If nfts[_id] is true, it means that an NFT with this ID already exists and the function will throw an error.
+
+  event Mint(address indexed _to, uint256 indexed _id);
+
+  function mintNFT(ApplicationForm memory applicationForm) public {
+    require(applicationForm.status == Status.Approved, 'Invalid approval');
 
     uint256 id = nextAvailableId;
     mint(msg.sender, id);
     applicationForms[id].ipfsLink = applicationForm.ipfsLink;
-    nextAvailableId ++;      //nextAvailableId variable to keep track of the next available ID for minting new NFTs.
+    nextAvailableId++; //nextAvailableId variable to keep track of the next available ID for minting new NFTs.
     emit Mint(msg.sender, id);
-}
-// function is responsible for minting a new NFT with the given ID and metadata taken from the ApplicationForm struct.
+  }
 
+  // function is responsible for minting a new NFT with the given ID and metadata taken from the ApplicationForm struct.
 
-event IdentityCardCreated(uint256 idCardId, IdCard idCard);
+  event IdentityCardCreated(uint256 idCardId, IdCard idCard);
 
-function createIdentityCard(uint256 applicationId, IdCard memory idCard)public onlyOwner {
-    require(applicationStatus[applicationId] == Status.Approved, "The application must be in the Approved status");
+  function createIdentityCard(uint256 applicationId, IdCard memory idCard) public onlyOwner {
+    require(
+      applicationStatus[applicationId] == Status.Approved,
+      'The application must be in the Approved status'
+    );
     uint256 idCardId = idCardCounter;
     idcards[idCardId] = idCard;
-    idCardCounter ++;
+    idCardCounter++;
     emit IdentityCardCreated(idCardId, idCard);
-}
+  }
 
-event PassportCreated(uint256 passportid, Passport passport);
+  event PassportCreated(uint256 passportid, Passport passport);
 
-function createPassport(uint256 applicationId, Passport memory passport)public onlyOwner {
-    require(applicationStatus[applicationId] == Status.Approved, "The application must be in the Approved status");
+  function createPassport(uint256 applicationId, Passport memory passport) public onlyOwner {
+    require(
+      applicationStatus[applicationId] == Status.Approved,
+      'The application must be in the Approved status'
+    );
     uint256 passportId = idpassportCounter;
     passports[passportId] = passport;
-    idpassportCounter ++;
+    idpassportCounter++;
     emit PassportCreated(passportId, passport);
-}
+  }
 
-event CreatedCarLicense(uint256 aplicationid, CarLicense carlicense);
+  event CreatedCarLicense(uint256 aplicationid, CarLicense carlicense);
 
-function createCarlicense(uint256 applicationId, CarLicense memory carlicense)public onlyOwner {
-    require(applicationStatus[applicationId] == Status.Approved, "The application must be in the Approved status");
+  function createCarlicense(uint256 applicationId, CarLicense memory carlicense) public onlyOwner {
+    require(
+      applicationStatus[applicationId] == Status.Approved,
+      'The application must be in the Approved status'
+    );
     uint256 carlicenseId = idcarlicenseCounter;
     carlicenses[carlicenseId] = carlicense;
-    idcarlicenseCounter ++;
+    idcarlicenseCounter++;
     emit CreatedCarLicense(carlicenseId, carlicense);
-}}
+  }
+}
